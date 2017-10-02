623ed4c665d5a3155cd55158a51bc98fa05aac3f
Annette Maza <nettymaza@gmail.com>
2017-10-02 16:13:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -5,16 +5,16 @@ class App < Sinatra::Base
     "Hello, World!"
   end
 
-  get '/name' do
-    "My name is Annette"
-  end
-
-  get '/hometown' do
-    "My hometown is Salt Lake City"
-  end
-
-  get '/favorite-song'
-    "My favorite song is Lower the Heavens"
-  end
+  # get '/name' do
+  #   "My name is Annette"
+  # end
+  # 
+  # get '/hometown' do
+  #   "My hometown is Salt Lake City"
+  # end
+  # 
+  # get '/favorite-song'
+  #   "My favorite song is Lower the Heavens"
+  # end
 
 end
