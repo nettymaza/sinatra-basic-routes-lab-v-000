9ae6c6d71875e2325de791511bd6029b76f8b1cd
Annette Maza <nettymaza@gmail.com>
2017-10-02 16:16:56 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Annette"
+  end
+
+  get '/hometown' do
+    "My hometown is Salt Lake City"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Lower the Heavens"
+  end
+
 end
